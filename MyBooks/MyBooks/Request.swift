e003720fe98978d520796d57c2cde1aa9357f7df
Suelen Critina Vaz <sucris12@gmail.com>
2022-09-08 14:34:11 -0300

    Create Network and Request

--- MyBooks/MyBooks/Request.swift
+++ MyBooks/MyBooks/Request.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+enum RequestMethod: String {
+    case create = "CREATE"
+    case read = "READ"
+    case update = "UPDATE"
+    case delete = "DELETE"
+}
+
+struct Request {
+    var url: String
+    var method: RequestMethod
+}
