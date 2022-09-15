40d24354d6f07e739fd60946dd5ac97af135f345
Suelen Critina Vaz <sucris12@gmail.com>
2022-09-15 10:06:57 -0300

    Create button for BookDetail, created too now.

---
+++ MyBooks/MyBooks/Scenes/BookDetail/BookDetail.swift
@@ -0,0 +1,20 @@
+//
+//  BookDetail.swift
+//  MyBooks
+//
+//  Created by Suh on 14/09/22.
+//
+
+import SwiftUI
+
+struct BookDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BookDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        BookDetail()
+    }
+}
